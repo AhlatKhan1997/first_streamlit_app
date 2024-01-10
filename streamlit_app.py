298420e5970e09a8523fde542b7ccc410453e4b5
AhlatKhan1997 <156167386+AhlatKhan1997@users.noreply.github.com>
2024-01-10 12:31:55 +0500

    Update streamlit_app.py
    typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dab
 fruit_list=fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruit_to_show)
+streamlit.dataframe(fruits_to_show)
 
